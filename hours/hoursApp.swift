857c38d5fb2b02e8be85d4e7ae693c0f87f9e64f
pablosan <pablo.sanchez@aerlingus.com>
2025-08-10 14:12:18 +0200

    Initial Commit

---
+++ hours/hoursApp.swift
@@ -0,0 +1,17 @@
+//
+//  hoursApp.swift
+//  hours
+//
+//  Created by pablosan on 10/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct hoursApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ hoursTests/hoursTests.swift
@@ -0,0 +1,17 @@
+//
+//  hoursTests.swift
+//  hoursTests
+//
+//  Created by pablosan on 10/08/2025.
+//
+
+import Testing
+@testable import hours
+
+struct hoursTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
